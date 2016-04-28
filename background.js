85ac34be2fcfea0316174bde6bdbba0a6bc1a7b9
Jon Wynveen <jwynveen@mpirik.com>
2016-04-28 17:16:03 -0500

    phone numbers resolves #5

--- background.js
+++ background.js
@@ -1,7 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log('Turning ' + tab.url + ' red!');
   chrome.tabs.executeScript({
-    // code: 'document.body.style.backgroundColor="red"'
     file: 'form-faker.js'
   });
 });
--- form-faker.js
+++ form-faker.js
@@ -1,4 +1,5 @@
 console.log('form-faker: faking data...');
+// debugger;
 const dictionary = getDictionary();
 var inputs = document.getElementsByTagName('input');
 
@@ -17,12 +18,15 @@ for (var i = 0; i < inputs.length; i++) {
     case 'lastName':
       input.value = faker.name.firstName();
       break;
+    case 'email':
+      input.value = faker.internet.email();
+      break;
+    case 'phone':
+      input.value = faker.phone.phoneNumber('(###) ###-####');
+      break;
     case 'date-past':
       var date = faker.date.past(50);
       input.value = date.toLocaleDateString();
-      break;
-    case 'email':
-      input.value = faker.internet.email();
       break;
     case 'account':
       input.value = faker.finance.account();
@@ -54,6 +58,33 @@ function getDictionary() {
     ['emailaddress', 'email'],
     ['email_address', 'email'],
     ['email-address', 'email'],
+    // phone
+    ['phone', 'phone'],
+    ['ph', 'phone'],
+    ['cellphone', 'phone'],
+    ['cell_phone', 'phone'],
+    ['cell-phone', 'phone'],
+    ['cellph', 'phone'],
+    ['cell_ph', 'phone'],
+    ['cell-ph', 'phone'],
+    ['mobilephone', 'phone'],
+    ['mobile_phone', 'phone'],
+    ['mobile-phone', 'phone'],
+    ['mobileph', 'phone'],
+    ['mobile_ph', 'phone'],
+    ['mobile-ph', 'phone'],
+    ['homephone', 'phone'],
+    ['home_phone', 'phone'],
+    ['home-phone', 'phone'],
+    ['homeph', 'phone'],
+    ['home_ph', 'phone'],
+    ['home-ph', 'phone'],
+    ['hmphone', 'phone'],
+    ['hm_phone', 'phone'],
+    ['hm-phone', 'phone'],
+    ['hmph', 'phone'],
+    ['hm_ph', 'phone'],
+    ['hm-ph', 'phone'],
     // date-past
     ['birthdate', 'date-past'],
     // account
