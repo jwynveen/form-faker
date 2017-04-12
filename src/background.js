213c45df477c00dd5b210ac088905ef0757f8296
Jon Wynveen <jwynveen@mpirik.com>
2017-04-12 09:05:10 -0500

    Inject additional scripts only once needed

--- src/background.js
+++ src/background.js
@@ -1,5 +1,12 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({
+  // Inject scripts only once extension button is clicked
+  chrome.tabs.executeScript(tab.id, {
+    file: 'lib/marvel-characters.js'
+  });
+  chrome.tabs.executeScript(tab.id, {
+    file: 'lib/superheros.js'
+  });
+  chrome.tabs.executeScript(tab.id, {
     file: 'form-faker.js'
   });
 });
