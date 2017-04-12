0249464ed7d3306037a9aa16493d4141d76a4106
Jim Geurts <jim@biacreations.com>
2017-04-11 22:08:15 -0500

    Use a dropdown instead of radio buttons for name generator

--- src/options.js
+++ src/options.js
@@ -9,7 +9,7 @@ function save_options() {
     mappings: mappings,
     dateFormat: document.getElementById('dateFormat').value || 'MM/DD/YYYY',
     emailPattern: document.getElementById('emailPattern').value,
-    nameGenerator: document.querySelector('input[name="nameGenerator"]:checked').value,
+    nameGenerator: document.getElementById('nameGenerator').value,
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -54,21 +54,12 @@ function restore_options() {
     mappings: [{}],
     dateFormat: 'MM/DD/YYYY',
     emailPattern: '',
+    nameGenerator: 'random',
   }, function(items) {
     mappings = items.mappings;
     document.getElementById('dateFormat').value = items.dateFormat;
     document.getElementById('emailPattern').value = items.emailPattern;
-    switch (items.nameGenerator) {
-      case 'marvel':
-        document.getElementById('nameGenerator-marvel').checked = true;
-        break;
-      case 'superhero':
-        document.getElementById('nameGenerator-superhero').checked = true;
-        break;
-      default:
-        document.getElementById('nameGenerator-random').checked = true;
-        break;
-    }
+    document.getElementById('nameGenerator').value = items.nameGenerator;
     loadMappings();
   });
 }
